d407d1a4e3a44088378f6bbc308922b5760d4552
Aayush Uppal <aayuppal@gmail.com>
2021-12-15 13:07:27 -0800

    update Wed, Dec 15, 2021  1:07:27 PM

--- lnkmngr/utils/consts.py
+++ lnkmngr/utils/consts.py
@@ -1,10 +1,14 @@
-def get_lnkmngr_db_path():
-    # from os import path
-    # BASE_DIR = path.abspath(path.dirname(__file__))
-    # return path.join(BASE_DIR, "lnkmngr.db") # ./lnkmngr/lnkmngr.db
+import os
 
-    # override
-    return "C:\\Users\\auppal\\dev\\pfin2\\pfin-input-data\\sqlite_db\\lnkmngr.db"
+
+def get_lnkmngr_db_path():
+    try:
+        return os.environ["DB_PATH"]
+    except KeyError:
+        from os import path
+
+        BASE_DIR = path.abspath(path.dirname(__file__))
+        return path.join(BASE_DIR, "../../lnkmngr.db")
 
 
 CATEGORY_TYPE = "category"
