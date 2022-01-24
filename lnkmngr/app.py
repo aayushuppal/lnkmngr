9148641551edfe55ec985e041d13d615b14ffce1
Aayush Uppal <aayuppal@gmail.com>
2022-01-23 20:20:41 -0800

    update Sun, Jan 23, 2022  8:20:39 PM

--- lnkmngr/app.py
+++ lnkmngr/app.py
@@ -1,6 +1,20 @@
 from flask import Flask
 from utils.config import get_port, get_expose_to_lan
+from pathlib import Path
+import shutil
+import glob
 
+def clean_tmp_files():
+    rootdir = Path.cwd()
+    tmp_mei_dirs = []
+    for path in glob.glob(f'{rootdir}/_MEI*'):
+        p = Path(path)
+        tmp_mei_dirs.append(
+            (p, p.stat().st_mtime)
+        )
+    tmp_mei_dirs = sorted(tmp_mei_dirs, key=lambda x: -x[1]) 
+    for x in tmp_mei_dirs[2:]:
+        shutil.rmtree(x[0], ignore_errors=True)
 
 def init_app():
     app = Flask(__name__)
@@ -18,5 +32,6 @@ def init_app():
     return app
 
 
+clean_tmp_files()
 app = init_app()
 app.run(port=get_port(), host="0.0.0.0" if get_expose_to_lan() else "")
